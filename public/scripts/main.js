d916d08c382c13eeb61f4265e373e0bab9e7cdcc
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-03-02 15:25:54 -0800

    adjusts number margins

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,12 +5,15 @@
 var canvas;
 var canWidth;
 var img;
-var x1 = 10;
-var y1 = 50;
-var y2 = 50;
-var y3 = 50;
-var x2 = 85;
-var x3 = 149;
+var x1 = -10;
+var y1 = 0;
+
+var y2 = 80;
+var y3 = 0;
+
+var x2 = 10;
+var x3 = 120;
+
 var currentImg;
 var currentNode = null;
 var imgW;
@@ -28,6 +31,7 @@ function draw() {
   strokeWeight(2);
   canWidth = canvas.width;
   noFill();
+  // console.log(mouseX, mouseY)
   rect(2, 30, canWidth - 5, canvas.height);
 }
 
@@ -46,25 +50,23 @@ function findImageUrlLocal(node) {
   result = result.substring(result.indexOf("0/") + 1);
 }
 
-function determineSize(resultImg) {
-  console.log(resultImg);
-  var randomNum = Math.random(1, 2);
-  imgW = resultImg.width * randomNum;
-  imgH = resultImg.height * randomNum;
+function determineSideSizes(resultImg, min, max) {
+  var factor = Math.random() * (max - min) + min;
+  imgW = resultImg.width * factor;
+  imgH = resultImg.height * factor;
 }
 
-function resizeImage(resultImg) {
-
-}
 
 function passAndRenderImage(result, lane) {
   loadImage(result, function(img) {
-    determineSize(img);
     if (lane == 1) {
+      determineSideSizes(img, 0.2, 0.35);
       image(img, x1, y1, imgW, imgH);
     } else if (lane == 2) {
+      determineSideSizes(img, 0.35, 0.45);
       image(img, x2, y2, imgW, imgH);
     } else {
+      determineSideSizes(img, 0.45, 0.6);
       image(img, x3, y3, imgW, imgH);
     }
   });
@@ -74,4 +76,4 @@ $(document).ready(function() {
     $(document).click(function(event) {
         currentNode = $(event.target);
     });
-}); // End document ready
+});
