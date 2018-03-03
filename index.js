a70123e71e2cfdf8e3a8e2a4139b86a94fb0038c
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-03-02 23:08:11 -0800

    moves file structure

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.sendFile('index.html'))
   .use(favicon(path.join(__dirname,'public','images','favicon.ico')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
